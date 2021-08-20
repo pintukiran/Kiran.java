5543192f29ec4e96638fb2a7db1f23b63ec179e5
Kiran S <kiranpintu633@gmail.com>
2021-08-20 21:50:54 +0530

    java=1

---
+++ Bootstrap/main.java
@@ -0,0 +1,19 @@
+public class main{
+	public static void main(String[] args){
+		int X=123;
+		double Y=3.14;
+		boolean Z=false;
+		float A=3.141592f;
+		char symbol='#';
+		String nmae="kiran";
+		
+		System.out.println("Hello "+X);
+		System.out.println("Hello "+Y);
+		System.out.println("Hello "+Z);
+		System.out.println("Hello "+A);
+		System.out.println("Hello "+symbol);
+		System.out.println("Hello "+nmae);
+		
+		
+	}
+}
