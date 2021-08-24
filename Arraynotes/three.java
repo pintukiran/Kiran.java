6e18f0ffac769ae8aec3b782ee85b0d63356003f
Kiran S <kiranpintu633@gmail.com>
2021-08-24 22:36:35 +0530

    Array

---
+++ Arraynotes/three.java
@@ -0,0 +1,10 @@
+class three{
+	public static void main(String[] args){
+		for(int i=1; i<=10; i=i+1)
+		
+		System.out.println("Eligible for all" +i);
+	
+}
+}
+
+//initilization execute only one time
