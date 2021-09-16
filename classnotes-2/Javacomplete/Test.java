9589e91bb49123637c9d8bc1fc48513ac5fef077
Kiran S <kiranpintu633@gmail.com>
2021-09-16 23:25:44 +0530

    Poly

---
+++ classnotes-2/Javacomplete/Test.java
@@ -0,0 +1,22 @@
+class text{
+    public void m1(){
+        System.out.println("Hello gud mrng");
+    }
+    public void m1(double b){
+        System.out.println("Kiran");
+    }
+    public void m1(int a){
+        System.out.println("two");
+    }
+    public void m2(){
+        System.out.println("Good ");
+    }
+
+public static void main(String[] A){
+    text t=new text();
+    t.m1(10.0000);
+    t.m1(1);
+    t.m1();
+    t.m2();
+    }
+}
