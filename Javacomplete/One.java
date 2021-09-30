9f28c90dc97fcd465298a10631448c160f8e40d0
Kiran S <kiranpintu633@gmail.com>
2021-09-30 06:03:22 +0530

    okie

--- Javacomplete/One.java
+++ Javacomplete/One.java
@@ -8,10 +8,12 @@ class B extends A{
 String k1(){
         System.out.println("B-Hello");
            return null;
+}
  }
-       
+ class text{   
     public static void main(String[] args){
         B b=new B();
         b.k1();
     }
-}
+    }
+
